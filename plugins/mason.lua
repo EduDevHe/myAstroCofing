85aba75fed9dceca79492099ac632cd460b1f939
Eduardo <edu.he.dev@gmail.com>
2023-05-01 15:14:47 -0300

    typescript is working

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -1,21 +1,26 @@
--- customize mason plugins
+-- w customize mason plugins
 return {
   -- use mason-lspconfig to configure LSP installations
   {
     "williamboman/mason-lspconfig.nvim",
     -- overrides `require("mason-lspconfig").setup(...)`
     opts = {
-       ensure_installed = { "svelte", "jdtls", "html", "angularls", "lua_ls", "eslint", "jedi_language_server", "intelephense"},
+      ensure_installed = { "svelte", "jdtls", "html", "angularls", "lua_ls", "eslint", "jedi_language_server",
+        "intelephense" },
     },
   },
   -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
+
+  -- overrides `require("mason-null-ls").setup(...)`
+
+
   {
     "jay-babu/mason-null-ls.nvim",
-    -- overrides `require("mason-null-ls").setup(...)`
     opts = {
-       ensure_installed = { "prettier", "stylua" },
+      ensure_installed = { "prettier", "stylua" },
     },
   },
+
   {
     "jay-babu/mason-nvim-dap.nvim",
     -- overrides `require("mason-nvim-dap").setup(...)`
